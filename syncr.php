78ca1506844fd687bb69c275dfd2ab48590784a7
Junior Grossi <juniorgro@gmail.com>
2015-11-23 13:02:15 -0200

    Add include of functions file

--- syncr.php
+++ syncr.php
@@ -5,6 +5,7 @@ use Symfony\Component\Console\Application;
 use Syncr\SyncCommand;
 
 require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/include/functions.php';
 
 $app = new Application('Syncr', '1.0');
 $app->add(new SyncCommand());
