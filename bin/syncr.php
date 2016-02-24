173c4d0e35a42e3cd45e156add4967886adcfc8a
Junior Grossi <juniorgro@gmail.com>
2016-02-24 12:18:12 -0300

    Change bin files to bin directory

--- bin/syncr.php
+++ bin/syncr.php
@@ -4,8 +4,8 @@
 use Symfony\Component\Console\Application;
 use Syncr\SyncCommand;
 
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/include/functions.php';
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../include/functions.php';
 
 $app = new Application('Syncr', '@package_version@');
 $app->add(new SyncCommand());
